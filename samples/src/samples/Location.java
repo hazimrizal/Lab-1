55f36d3a9c53037a2a93b5ee075d4d09ef2d4fac
Hazim Rizal <hazim705@gmail.com>
2021-03-18 16:52:12 +0800

    Location Commit

---
+++ samples/src/samples/Location.java
@@ -0,0 +1,13 @@
+package samples;
+
+public class Location {
+	private String location;
+
+	public String getLocation() {
+		return location;
+	}
+
+	public void setLocation(String location) {
+		this.location = location;
+	}
+}
