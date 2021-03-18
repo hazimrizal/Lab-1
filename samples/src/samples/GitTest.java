31ce979019044470d73488a48128956cc0be79d2
Hazim Rizal <hazim705@gmail.com>
2021-03-18 16:14:54 +0800

    Initiate Commit

--- samples/src/samples/GitTest.java
+++ samples/src/samples/GitTest.java
@@ -3,6 +3,7 @@ package samples;
 public class GitTest {
 	public static void main(String[] args) {
 		
-		System.out.print("Git is fun");
+		System.out.println("Git is fun");
+		System.out.print("Git is cool");
 	}
 }
